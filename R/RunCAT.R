b3a913e6ad29abe281d8a4994c71f46b4a13f97c
hspark <hspark@water.re.kr>
2015-08-14 02:42:22 +0900

    예제 스크립트 추가

--- R/RunCAT.R
+++ R/RunCAT.R
@@ -3,13 +3,18 @@
 #' RunCAT 모형 구동 함수.
 #' @param infile 입력파일의 경로
 #' @param outfile 출력파일의 경로
-#' @param format 포맷
+#' @param format 포맷, "" 일 경우 바이너리 형태로 저장되며,
+#' "[*|*]" 일 경우 텍스트 형태로 저장됨
 #' @return NULL
-#' @author Heeseong Park \email{hspark90@@i-fam.net}
+#' @author 박희성 \email{hspark90@@i-fam.net}
 #' @useDynLib rcat
 #' @import Rcpp
 #' @encoding UTF-8
 #' @export
+#' @examples
+#' download.file("http://r.prj.kr/data/rcat/rcat.zip", "rcat.zip")
+#' unzip("rcat.zip")
+#' RunCAT("dist_101208.txt", "dist_101208.out")
 RunCAT <- function(infile, outfile, format = "[*:*]")
 {
   if(missing(infile) || missing(outfile))
