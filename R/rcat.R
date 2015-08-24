b4a951d5ac80c9c156c010ec2ec7553f714c7d76
hspark <hspark@water.re.kr>
2015-08-24 10:50:00 +0900

    버전 변경 ver 0.1.1

--- R/rcat.R
+++ R/rcat.R
@@ -5,7 +5,7 @@
 #' \tabular{ll}{
 #' Package: \tab rcat\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.1.0\cr
+#' Version: \tab 0.1.1\cr
 #' Date: \tab 2015-08-12\cr
 #' License: \tab GPL-2\cr
 #' }
