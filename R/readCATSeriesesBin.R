8635e2e9464539f9679d97b08ae04c26d77ea99b
hspark <hspark@water.re.kr>
2015-09-10 08:44:09 +0900

    바이너리 파일 읽고 쓰기 랩핑

---
+++ R/readCATSeriesesBin.R
@@ -0,0 +1,27 @@
+#' readBinCATSerieses
+#'
+#' CAT 모형에서 사용하는 변수계열을 저장한 Binary 파일을 읽어오는 함수.
+#' @param filename 출력파일의 경로
+#' @return rcat 모형에서 사용하는 "rcat_serieses" 클래스의 변수
+#' @author 박희성 \email{hspark90@@i-fam.net}
+#' @useDynLib rcat
+#' @import Rcpp
+#' @encoding UTF-8
+#' @export
+#' @examples
+#' download.file("http://r.prj.kr/data/rcat/rcat_ex.zip", "rcat_ex.zip")
+#' unzip("rcat_ex.zip")
+readBinCATSerieses <- function(filename)
+{
+  if(missing(filename))
+    stop("filename must be needed!")
+  ret <- read_cat_serieses(path.expand(filename[1]))
+  for(i in 1:length(ret))
+    if(!is.null(colnames(ret[[i]]))) Encoding(colnames(ret[[i]])) <- "UTF-8"
+  #cls <- class(ret)
+  #lapply(ret, function(x) {
+  #  if(!is.null(colnames(x))) Encoding(colnames(x)) <- "UTF-8"
+  #  x})
+  #class(ret) <- cls
+  return(ret)
+}
---
+++ R/writeCATSeriesesBin.R
@@ -0,0 +1,22 @@
+#' writeBinCATSerieses
+#'
+#' CAT 모형에서 사용하는 변수계열을 Binary 파일로 저장하는 함수 .
+#' @param x rcat 모형에서 사용하는 "rcat_serieses" 클래스의 변수
+#' @param filename 출력파일의 경로
+#' @return 에러 메시지나 저장된 파일이름(보이지 않음)
+#' @author 박희성 \email{hspark90@@i-fam.net}
+#' @useDynLib rcat
+#' @import Rcpp
+#' @encoding UTF-8
+#' @export
+#' @examples
+#' download.file("http://r.prj.kr/data/rcat/rcat_ex.zip", "rcat_ex.zip")
+#' unzip("rcat_ex.zip")
+writeBinCATSerieses <- function(x, filename)
+{
+  if(missing(filename))
+    stop("filename must be needed!")
+  if(!(any(class(x) == "rcat_serieses")))
+    stop("Error: x is not 'rcat_serieses' object!!")
+  invisible(write_cat_serieses(path.expand(filename[1]), x))
+}
