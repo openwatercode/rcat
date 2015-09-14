acdeed29af722c3211b45f7f4d5ccff4a33954df
hspark <hspark@water.re.kr>
2015-09-14 09:56:19 +0900

    외부인자를 받아서 수행하도록 메인루틴 변경

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 string runcat(char* infile, char* outfile, char* format);
 int main(int argc, char *argv[])
 {
-    char* input = (char *)"test_data/dist_101208-1.txt";
-    char* output = (char *)"test_data/test_cb1.out";
-    char* format = (char *)"[*|*]";
+    char* input = argv[1]; //(char *)"test_data/dist_101208-1.txt";
+    char* output = argv[2]; //(char *)"test_data/test_cb1.out";
+    char* format = argv[3]; //(char *)"[*|*]";
     string ret;
     ret = runcat(input, output, format);
     return 0;
